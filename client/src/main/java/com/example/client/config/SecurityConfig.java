2d5cf91aed5605e28214720fe8e6012ccc67115c
Jabborov Egamberdi <jabborovegamberdi2002@gmail.com>
2025-01-13 19:37:29 +0500

    Init

--- client/src/main/java/com/example/client/config/SecurityConfig.java
+++ client/src/main/java/com/example/client/config/SecurityConfig.java
@@ -4,19 +4,45 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest()
+//                        .authenticated()
+//                )
+//                .oauth2Login(Customizer.withDefaults());
+//        return http.build();
+//    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+        String baseUri = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI;
+        var resolver = new DefaultOAuth2AuthorizationRequestResolver(clientRegistrationRepository, baseUri);
+        resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/")
+                        .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(oauth2Login -> {
+                    oauth2Login.loginPage(baseUri + "/oidc-client");
+                    oauth2Login.authorizationEndpoint(authorizationEndpoint ->
+                            authorizationEndpoint.authorizationRequestResolver(resolver)
+                    );
+                })
+                .oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 
